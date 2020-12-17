34bf52c5aaef26b648479218f7893f82d92962dc
mostafamedht98 <mostafamedht98"gmail>
2020-12-17 14:04:40 +0200

    Api With passport

---
+++ oauth2_client/auth_redirection.php
@@ -0,0 +1,9 @@
+<?php
+$query = http_build_query(array(
+    'client_id' => '1',
+    'redirect_uri' => 'http://localhost/oauth2_client/callback.php',
+    'response_type' => 'code',
+    'scope' => '',
+));
+ 
+header('Location: http://laravelsiglesignon2.online/oauth/authorize?'.$query);
